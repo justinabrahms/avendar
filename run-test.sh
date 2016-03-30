dab77273deb5d22dfd4eb3646269dcba904196e5
Justin Abrahms <justin@abrah.ms>
2016-03-30 12:13:54 -0400

    Save test output.

--- run-test.sh
+++ run-test.sh
@@ -2,5 +2,7 @@
 # Builds and runs the relevant test files. If it's not picking
 # something up, ensure you've listed it in the TESTS var in the
 # Makefile.
+TEST_OUTPUT_DIR="${CIRCLE_TEST_REPORTS:-.}"
+mkdir -p $TEST_OUTPUT_DIR
 make test
-find . -name "*_test" -exec ./{} \;
+find . -name "*_test" -exec ./{} --gtest_output=xml:$TEST_OUTPUT_DIR/{}.xml \;
