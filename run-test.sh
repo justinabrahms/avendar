0c83b5c3917bfcb33393b27fd6749db1746c7a04
Justin Abrahms <justin@abrah.ms>
2016-03-29 23:25:32 -0400

    Note in run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Builds and runs the relevant test files. If it's not picking
+# something up, ensure you've listed it in the TESTS var in the
+# Makefile.
 make test
 find . -name "*_test" -exec ./{} \;
